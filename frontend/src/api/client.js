a07608fa4afd2322ff66ca6c33f664235b24000e
Paruhang Limbu <limbuparuhang1@gmail.com>
2026-05-10 10:27:38 +0545

    frontend fix deploy

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://patient-dashboard-api.onrender.com',
   timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
 })
