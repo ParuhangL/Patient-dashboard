3e73c11272fc7614015e37ad9653dfd7b0e98972
Paruhang Limbu <limbuparuhang1@gmail.com>
2026-06-17 17:35:17 +0545

    forgot

--- backend/build.sh
+++ backend/build.sh
@@ -4,3 +4,13 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth.models import User
+u = User.objects.filter(is_staff=True).first()
+if u:
+    u.set_password('Admin1234')
+    u.save()
+    print(f'Password reset for {u.username}')
+else:
+    print('No staff user found')
+"
